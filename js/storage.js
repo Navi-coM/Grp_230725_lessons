59d9b5422fb0250141ad8c66552caa38d1afd53c
Navi-coM <navicom17@gmail.com>
2025-11-11 12:54:38 +0200

    Update new code

--- js/storage.js
+++ js/storage.js
@@ -51,18 +51,80 @@ console.log(JSON.parse(localStorage.getItem('userDataIsFrontEnd')));
 
 localStorage.removeItem('studentList');
 // localStorage.clear();
-console.log(localStorage.length);
+// console.log(localStorage.length);
 
-if(localStorage.length !== 0) {
-    let someKeyData = JSON.parse(localStorage.getItem(localStorage.key(2)));
-    let someKey = localStorage.key(localStorage.length - 1);
-    console.log(someKey);
-} else {
-    console.log(`LocalStorage is Empty!`);
-}
+// if(localStorage.length !== 0) {
+//     let someKeyData = JSON.parse(localStorage.getItem(localStorage.key(2)));
+//     let someKey = localStorage.key(localStorage.length - 1);
+//     console.log(someKey);
+// } else {
+//     console.log(`LocalStorage is Empty!`);
+// }
 
 // ----------------------- Отримання наявних ключів у storage ---------
 
 let storageKeys = Object.keys(localStorage);
 
-console.log(storageKeys);
+// console.log(storageKeys);
+
+// ---------------------------- Click Storage -----
+
+const btnClick = document.querySelector('#btn-click');
+const btnClearStore = document.querySelector('#btn-clear');
+const clickCountTitle = document.querySelector('#click-count');
+
+let clickCount = localStorage.getItem('clickCounter') || 0;
+
+function checkClickCountStorage() {
+    clickCountTitle.innerHTML = clickCount;
+}
+
+function clickCountStorage() {
+    clickCount++;
+    localStorage.setItem('clickCounter', clickCount);
+
+    let clickStorageCount = localStorage.getItem('clickCounter');
+    clickCountTitle.innerHTML = clickStorageCount;
+}
+
+function clrearCountStorage() {
+    localStorage.removeItem('clickCounter');
+    clickCountTitle.innerHTML = clickCount = 0;
+}
+
+document.addEventListener('DOMContentLoaded', checkClickCountStorage);
+btnClick.addEventListener('click', clickCountStorage);
+btnClearStore.addEventListener('click', clrearCountStorage);
+
+// -----------------------------Емуляція зміним теми за допомогою localstorage ------
+
+const btnChangeTheme = document.querySelector('#btn-change-theme');
+const btnResetTheme = document.querySelector('#btn-reset-theme');
+const blockBody = document.body;
+
+function changeTheme() {
+    blockBody.classList.toggle('body_dark');
+
+    if (blockBody.classList.contains('body_dark')) {
+        localStorage.setItem('pageTheme', 'dark')
+    } else {
+        localStorage.setItem('pageTheme', 'light')
+    }
+}
+function resetPageTheme() {
+    if (blockBody.classList.contains('body_dark')) {
+        localStorage.removeItem('pageTheme');
+        blockBody.classList.remove('body_dark');
+    }
+}
+function checkPageThemeStorage() {
+    const savedTheme = localStorage.getItem('pageTheme');
+
+    if (savedTheme === 'dark') {
+        blockBody.classList.add('body_dark');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', checkPageThemeStorage);
+btnChangeTheme.addEventListener('click', changeTheme);
+btnResetTheme.addEventListener('click', resetPageTheme);
